a5bf5216a109a7304ffcb48d41b8ed2ca21d4b73
ciocaion <73889009+ciocaion@users.noreply.github.com>
2022-05-27 16:05:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
+const numberOfSlides = slides.length;
+var slideNumber = 0;
+
+nextBtn.addEventListener("click", () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+
+    if(slideNumber > (numberOfSlides - 1)){
+        slideNumber = 0;
+    }
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+});
+
+prevBtn.addEventListener("click", () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    slideNumber--;
+
+    if(slideNumber < 0){
+        slideNumber = numberOfSlides - 1;
+    }
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+});
